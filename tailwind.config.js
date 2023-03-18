cbee5794e241efeb2d263ea3e378531520302e68
Vedang Manerikar <ved.manerikar@gmail.com>
2023-03-18 20:23:01 +0530

    Install TailwindCSS
    I've followed installation instructions from Tailwind's Get Started
    section: https://tailwindcss.com/docs/installation and committed all
    the files created as a result.
    
    I have also installed the following plugins:
    
    1. Tailwind Typography from
    https://tailwindcss.com/docs/typography-plugin
    
    2. A component plugin to add styling for the Table of Contents as
    generated by `ox-neuron`. I followed the instructions for doing this
    from https://tailwindcss.com/docs/plugins

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./resources/**/*.{html, js}", "./public/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".toc .heading": {
+          "font-weight": 1000,
+          "font-size": "1.25em",
+        },
+      });
+    }),
+    require("@tailwindcss/typography"),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/line-clamp'),
+    // require('@tailwindcss/aspect-ratio'),
+  ],
+};
