87e3e445c9afbd4ec95c91de6e6a266ff0d34e03
Vedang Manerikar <ved.manerikar@gmail.com>
2023-05-09 15:38:17 +0530

    Move all the content to a Content component
    The reason for moving it here is that I eventually want to write code
    that generates the content from Emacs + ox-neuron here. (So that I do
    not have to keep manual track of the files from which various types of
    data are generated)

---
+++ components/content/src/me/vedang/content/interface.clj
@@ -0,0 +1,5 @@
+(ns me.vedang.content.interface)
+
+;;; The code that will eventually go here will be the ability to drop
+;;; into the actually org-mode files and generate the markdown output
+;;; by running Emacs + Ox-Neuron.
---
+++ components/content/test/me/vedang/content/interface_test.clj
@@ -0,0 +1,6 @@
+(ns me.vedang.content.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [me.vedang.content.interface :as content]))
+
+(deftest dummy-test
+  (is (= 1 1)))
