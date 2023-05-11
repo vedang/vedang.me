86f073bae993526194b03b71e736c067d28e51c5
Vedang Manerikar <ved.manerikar@gmail.com>
2023-05-11 16:13:29 +0530

    Add a footer to the base.html file
    - A creative commons license.
    - A call out to the tech I use.

--- components/render/src/me/vedang/render/post.clj
+++ components/render/src/me/vedang/render/post.clj
@@ -1,12 +1,25 @@
 (ns me.vedang.render.post
   (:require
+   [clojure.string :as cstr]
    [me.vedang.render.util :as util]
    [selmer.parser :as selmer]))
 
 (defn add-html-body
   [{:keys [metadata html] :as html-map} opts]
-  (assoc html-map
-         :body (-> opts
-                   :templates-dir
-                   (util/read-template "post.html")
-                   (selmer/render (assoc metadata :body html)))))
+  (let [post-opts (cond-> opts
+                    true (merge metadata)
+
+                    (seq (str html)) (assoc :body html)
+
+                    (seq (:parents metadata))
+                    (assoc :parents_str (cstr/join ", " (:parents metadata)))
+
+                    (seq (:children metadata))
+                    (assoc :children_str (cstr/join ", " (:children metadata)))
+
+                    (seq (:tags metadata))
+                    (assoc :tags_str (cstr/join ", " (:tags metadata))))]
+    (assoc html-map :body (-> post-opts
+                              :templates-dir
+                              (util/read-template "post.html")
+                              (selmer/render post-opts)))))
